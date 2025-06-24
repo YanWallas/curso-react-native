576786a0d9f6d069cdc8bb85dd6c7a8f2ca5b7cb
Yan Wallas <yanwallas98@gmail.com>
2025-06-24 16:49:07 -0300

    salvando na memoria do celular

--- myProject/App.js
+++ myProject/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { StatusBar, Platform } from 'react-native';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard } from 'react-native';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default class App extends Component{
 
@@ -10,6 +12,32 @@ export default class App extends Component{
       input: '',
       nome: ''
     };
+
+    this.gravaNome = this.gravaNome.bind(this);
+  }
+  
+  //ComponentDiMount - Quando o componente é montado em tela, ele é chamado
+  async componentDidMount(){
+    await AsyncStorage.getItem('nome').then((value) => {
+      this.setState({nome: value});
+    })
+  }
+
+  // Toda vez que um state é atualizado ele faz algo.
+  async componentDidUpdate(_, prevState){
+    const nome = this.state.nome;
+
+    if(prevState !== nome){
+      await AsyncStorage.setItem('nome', nome);
+    }
+  }
+
+  gravaNome(){
+    this.setState({
+      nome: this.state.input
+    });
+    alert('Salvo com sucesso!')
+    Keyboard.dismiss();
   }
 
   render(){
@@ -24,7 +52,7 @@ export default class App extends Component{
           underlineColorAndroid="transparent"
         />
 
-        <TouchableOpacity>
+        <TouchableOpacity onPress={this.gravaNome}>
           <Text style={styles.botao}>+</Text>
         </TouchableOpacity>
       </View>
