a45f0d88f4adfd4b739702fc53a114282a8df998
Yan Wallas <yanwallas98@gmail.com>
2025-07-11 02:20:55 -0300

    Criando Loading

--- prime_films/App.js
+++ prime_films/App.js
@@ -1,11 +1,12 @@
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, ActivityIndicator } from 'react-native';
 
 import api from './src/services/api';
 import Filmes from './src/Filmes';
 
 export default function App() {
   const [filmes, setFilmes] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
 
@@ -13,23 +14,33 @@ export default function App() {
       const response = await api.get('r-api/?api=filmes');
       //console.log(response.data);
       setFilmes(response.data);
+      setLoading(false);
     }
 
     loadFilmes();
 
   }, [])
+  
+  if(loading){
+    return(
+      <View style={{ alignItems:'center', justifyContent:'center', flex:1}}>
+        <ActivityIndicator color='#121212' size={45}/>
+      </View>
+    )
+  }else{
+    return (
+      <View style={styles.container}>
+        
+        <FlatList
+          data={filmes}
+          keyExtractor={item => String(item.id) }
+          renderItem={({item}) => <Filmes data={item}/> }
+        />
 
-  return (
-    <View style={styles.container}>
-      
-      <FlatList
-        data={filmes}
-        keyExtractor={item => String(item.id) }
-        renderItem={({item}) => <Filmes data={item}/> }
-      />
-
-    </View>
-  );
+      </View>
+    );
+  }
+ 
 }
 
 const styles = StyleSheet.create({
