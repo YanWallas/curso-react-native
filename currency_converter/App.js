ee06ab3ab26bb26850821e2705a60899f606753e
Yan Wallas <yanwallas98@gmail.com>
2025-07-18 17:04:08 -0300

    installing and structuring the Picker

--- currency_converter/App.js
+++ currency_converter/App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { PickerItem } from './src/Picker';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{color: '#FFF'}}>Open up App.js to start working on your app!</Text>
+      <View style={styles.areaMoeda}>
+        <Text style={styles.titulo}>Selecione sua moeda</Text>
+
+        <PickerItem/>
+
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +22,20 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#121212',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 40
   },
+  areaMoeda:{
+    backgroundColor: "#f9f9f9",
+    width: '90%',
+    borderTopLeftRadius: 8,
+    borderTopRightRadius: 8,
+    padding: 8
+  },
+  titulo:{
+    fontSize: 16,
+    color: '#000',
+    fontWeight: '500',
+    paddingLeft: 5,
+    paddingTop: 5
+  }
 });
