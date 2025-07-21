5f26ae5dcb94679a2860e7b58a89fabd9f8926de
Yan Wallas <yanwallas98@gmail.com>
2025-07-21 17:53:19 -0300

    configure base url and styling the app

--- viaCep/App.js
+++ viaCep/App.js
@@ -1,20 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, SafeAreaView } from 'react-native';
+import api from './src/services/api';
 
 export default function App() {
+  const [cep, setCep] = useState('');
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={{alignItems: 'center'}}>
+        <Text style={styles.text}>Digite um CEP desejavel:</Text>
+
+        <TextInput
+          style={styles.input}
+          placeholder="Ex: 79003241"
+          value={cep}
+          onChangeText={(text) => setCep(text)}
+          keyboardType='numeric'
+        />
+      </View>
+
+      <View style={styles.areaBtn}>
+        <TouchableOpacity style={[styles.btn, {backgroundColor: '#1d75cd'}]}>
+          <Text style={styles.btnText}>Buscar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={[styles.btn, {backgroundColor: '#cd3e1d'}]}>
+          <Text style={styles.btnText}>Limpar</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.resultado}>
+        <Text style={styles.itemText}>CEP: 465165168</Text>
+        <Text style={styles.itemText}>Logradouro: Rua U-64</Text>
+        <Text style={styles.itemText}>Bairro: Setor União</Text>
+        <Text style={styles.itemText}>Cidade: Goiânia</Text>
+        <Text style={styles.itemText}>Estado: GO</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 30
   },
+  text:{
+    marginTop: 25,
+    marginBottom: 15,
+    fontSize: 25,
+    fontWeight: 'bold'
+  },
+  input: {
+    backgroundColor: "#FFF",
+    borderWidth: 1,
+    borderColor: '#DDD',
+    borderRadius: 5,
+    width: '90%',
+    padding: 10,
+    fontSize: 18
+  },
+  areaBtn:{
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginTop: 15,
+    justifyContent: 'space-around'
+  },
+  btn:{
+    justifyContent: 'center',
+    alignItems: "center",
+    padding: 15,
+    borderRadius: 5,
+  },
+  btnText:{
+    fontSize: 20,
+    color: "#FFF"
+  },
+  resultado:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  itemText:{
+    fontSize: 22
+  }
 });
