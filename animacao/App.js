17f5fd5778777f9b80d71d6ae07a6fabea223266
Yan Wallas <yanwallas98@gmail.com>
2025-08-20 15:24:18 -0300

    using interpolate to use percentage of screen in width.

--- animacao/App.js
+++ animacao/App.js
@@ -2,9 +2,9 @@ import React, { useRef, useEffect} from 'react';
 import { StyleSheet, Text, View, Animated } from 'react-native';
 
 export default function App() {
-  const larAnimada = useRef(new Animated.Value(150)).current;
+  const larAnimada = useRef(new Animated.Value(0)).current;
   const altAnimada = useRef(new Animated.Value(50)).current;
-  const opacidadeAnimada = useRef(new Animated.Value(0)).current;
+  //const opacidadeAnimada = useRef(new Animated.Value(0)).current;
 
   useEffect(() => {
     // ANIMAÇÃO SEQUENCIAL (FAZ UMA, DEPOIS A OUTRA)
@@ -42,60 +42,72 @@ export default function App() {
     // ]).start();
     //********************************************************** */
 
-    // ANIMAÇÃO EM LOOP
-    // ANIMAÇÃO SEQUENCIAL E PARALELA JUNTAS:
-    // COMEÇA COM A SEQUENCIA
-    Animated.loop(
-      Animated.sequence([
-        Animated.timing(opacidadeAnimada, {
-          toValue: 1,
-          duration: 2000,
-          useNativeDriver: false
-        }),
-        //DEPOIS CHAMA O BLOCO DE PARALELA
-        Animated.parallel([
-          Animated.timing(larAnimada, {
-            toValue: 300,
-            duration: 2000,
-            useNativeDriver: false
-          }),
-          Animated.timing(altAnimada, {
-            toValue: 300,
-            duration: 2000,
-            useNativeDriver: false
-          }),
-        ]),
-        // CONTINUA A PARALELA, VOLTANDO PARA O ORIGINAL, UTILIZANDO O LOOṔ
-        Animated.parallel([
-          Animated.timing(larAnimada, {
-            toValue: 150,
-            duration: 2000,
-            useNativeDriver: false
-          }),
-          Animated.timing(altAnimada, {
-            toValue: 150,
-            duration: 2000,
-            useNativeDriver: false
-          }),
-        ])
-      ])
-    ).start();
+    // // ANIMAÇÃO EM LOOP
+    // // ANIMAÇÃO SEQUENCIAL E PARALELA JUNTAS:
+    // // COMEÇA COM A SEQUENCIA
+    // Animated.loop(
+    //   Animated.sequence([
+    //     Animated.timing(opacidadeAnimada, {
+    //       toValue: 1,
+    //       duration: 2000,
+    //       useNativeDriver: false
+    //     }),
+    //     //DEPOIS CHAMA O BLOCO DE PARALELA
+    //     Animated.parallel([
+    //       Animated.timing(larAnimada, {
+    //         toValue: 300,
+    //         duration: 2000,
+    //         useNativeDriver: false
+    //       }),
+    //       Animated.timing(altAnimada, {
+    //         toValue: 300,
+    //         duration: 2000,
+    //         useNativeDriver: false
+    //       }),
+    //     ]),
+    //     // CONTINUA A PARALELA, VOLTANDO PARA O ORIGINAL, UTILIZANDO O LOOṔ
+    //     Animated.parallel([
+    //       Animated.timing(larAnimada, {
+    //         toValue: 150,
+    //         duration: 2000,
+    //         useNativeDriver: false
+    //       }),
+    //       Animated.timing(altAnimada, {
+    //         toValue: 150,
+    //         duration: 2000,
+    //         useNativeDriver: false
+    //       }),
+    //     ])
+    //   ])
+    // ).start();
+
+    Animated.timing(larAnimada, {
+      toValue: 100,
+      duration: 4000,
+      useNativeDriver: false
+    }).start();
   }, []);
+
+  //usando interpolate para usar porcentagem de tela na largura.
+  let porcentagemLargura = larAnimada.interpolate({
+    inputRange: [0, 100], //Entrada, vai que quanto a quanto.
+    outputRange: ['0%', '100%'] //Vai sair 0% até 100%
+  })
 
   return (
     <View style={styles.container}>
 
       <Animated.View
         style={{
-          width: larAnimada,
+          width: porcentagemLargura,
           height: altAnimada,
           backgroundColor: '#4169e1',
           justifyContent: 'center',
-          opacity: opacidadeAnimada,
+          //opacity: opacidadeAnimada,
           borderRadius: 50
         }}
       >
-        <Text style={{ textAlign: 'center', fontSize: 22, color: '#FFF'}}>Carregando...</Text>
+        {/* <Text style={{ textAlign: 'center', fontSize: 22, color: '#FFF'}}>Carregando...</Text> */}
       </Animated.View>
 
     </View>
