922bd966fe543e8626c2f0a0c090540605f1fd7f
Yan Wallas <yanwallas98@gmail.com>
2025-08-28 16:29:54 -0300

    using icons

--- appIcons/App.js
+++ appIcons/App.js
@@ -1,9 +1,15 @@
 import { StyleSheet, Text, View } from 'react-native';
 
+import Feather from '@expo/vector-icons/Feather';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      
+      <Feather
+        name='users'
+        size={45}
+        color="#FF0000"
+      />
     </View>
   );
 }
